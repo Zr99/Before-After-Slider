b4790ac9955f4b2a762d16f616cbbde40a6a0ce9
Zr99 <38181091+Zr99@users.noreply.github.com>
2021-08-19 19:39:02 +1200

    Add files via upload

---
+++ java.js
@@ -0,0 +1,29 @@
+const slider = document.querySelector(".slider");
+const before = document.querySelector(".img-container-before");
+const after = document.querySelector(".img-container-after");
+const container = document.querySelector(".container");
+
+
+
+
+const drag = (e)=>{
+  let xPos = e.layerX;
+  let size = container.offsetWidth;
+
+ before.style.width = xPos + "px";
+ slider.style.left = xPos + "px";
+
+ if(xPos < 50){
+  before.style.width =0;
+  slider.style.left = 0;
+ }
+ 
+ if(xPos + 50 > size){
+   before.style.width = size + "px";
+   slider.style.left = size + "px";
+ }
+};
+
+// touchmove for mobile
+container.addEventListener('mousemove',drag);
+
